a289f2a1b8a9cbffdc103fba8477edba5d5dcf8b
egoist <0x142857@gmail.com>
2015-12-23 00:06:30 +0800

    apply loadConfig

--- routes/karma.js
+++ routes/karma.js
@@ -1,16 +1,11 @@
-var path = require('path')
 var deepAssign = require('deep-assign')
 var Server = require('karma').Server
-var pathExists = require('path-exists')
-var requireJSON = require('lazy-json').requireJSON
+var loadConfig = require('../lib/loadConfig')
 var config = require('../conf/karma.conf.js')
 
 module.exports = function () {
-  var customConfigFile = path.join(process.cwd(), '.ntrrc')
-  if (pathExists.sync(customConfigFile)) {
-    var customConfig = requireJSON(customConfigFile, true)
-    config = deepAssign({}, config, customConfig.karma)
-  }
+  var customConfig.karma = loadConfig(),karma
+  config = deepAssign({}, config, customConfig)
   if (argv.w || argv.watch) {
     config.autoWatch = true
     config.singleRun = false
